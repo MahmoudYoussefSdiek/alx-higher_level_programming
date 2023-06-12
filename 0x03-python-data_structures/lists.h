7dc7d60b010e65f06dffa897a52df10d07761b38
Mahmoud Youssef <111357017+MahmoudYoussefSdiek@users.noreply.github.com>
2023-06-12 15:30:39 +0300

    Fix code in lists.h file

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -17,6 +17,9 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+int is_palindrome(listint_t **head);
+void reverse_list(listint_t **head);
+int compare_lists(listint_t *head1, listint_t *head2);
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
