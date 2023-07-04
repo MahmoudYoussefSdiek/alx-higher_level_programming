8f0bc49411ed3a8a2daf73ebc39231d9c74cb543
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-07-04 22:57:10 +0000

    Fix docs in 101-locked_class.py file

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""locked class."""
+
+
 class LockedClass:
-    __slots__ = ['first_name']
+    """
+    attributes called 'first_name'.
+    """
+
+    __slots__ = ["first_name"]
