3db04b97674689f5dd7b91c75943f089ed008e68
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-07-19 16:58:34 +0000

    Add display() method to rectangle.py file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -93,3 +93,8 @@ class Rectangle(Base):
     def area(self):
         """Returns the area value of the Rectangle instance"""
         return self.width * self.height
+
+    def display(self):
+        """Prints the Rectangle instance with the character #"""
+        for _ in range(self.height):
+            print("#" * self.width)
