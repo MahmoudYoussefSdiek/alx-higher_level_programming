1bf257670729fde72dace7b581db4505ea23a113
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-07-19 19:37:11 +0000

    Add update() method to rectangle.py file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -107,3 +107,10 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.x, self.y, self.width, self.height
         )
+
+    def update(self, *args):
+        """Assigns an argument to each attribute"""
+        if args:
+            attributes = ["id", "width", "height", "x", "y"]
+            for i in range(len(args)):
+                setattr(self, attributes[i], args[i])
