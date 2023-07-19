99a3c1510ab9fe7b69f350807fb569a911dbf2d8
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-07-19 18:36:34 +0000

    Update display() method to rectangle.py file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -96,7 +96,10 @@ class Rectangle(Base):
 
     def display(self):
         """Prints the Rectangle instance with the character #"""
+        for _ in range(self.y):
+            print()
         for _ in range(self.height):
+            print(" " * self.x, end="")
             print("#" * self.width)
 
     def __str__(self):
