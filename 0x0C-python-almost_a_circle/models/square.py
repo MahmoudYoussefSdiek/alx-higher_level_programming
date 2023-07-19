b9a896ec1c80812ad889518f3fbba61ab0e56b32
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-07-19 20:25:31 +0000

    Create square.py file

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""This module defines the Square class"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class that inherits from the Rectangle class.
+
+    Attributes:
+        size (int): The size of the square.
+        x (int): The x-coordinate of the square.
+        y (int): The y-coordinate of the square.
+        id (int): The id of the square.
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+            x (int, optional): The x-coordinate of the square. Defaults to 0.
+            y (int, optional): The y-coordinate of the square. Defaults to 0.
+            id (int, optional): The id of the square. Defaults to None.
+        """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Getter for the size attribute"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for the size attribute"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Returns a string representation of the Square instance"""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
