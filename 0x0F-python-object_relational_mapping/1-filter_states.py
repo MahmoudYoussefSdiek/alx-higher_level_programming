9c98ca42bff15dcbbdf351ed88299201f9fafc7a
Mahmoud Youssef <111357017+MahmoudYoussefSdiek@users.noreply.github.com>
2023-09-15 22:51:48 +0300

    Fix code in 1-filter_states.py file

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
-
 """
 This script lists all states with a name starting with N (upper N)
 from the database hbtn_0e_0_usa.
 """
+
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
     username = sys.argv[1]
