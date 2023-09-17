1bc02733ba462ce749243c185371ab095ef79382
Mahmoud Youssef <111357017+MahmoudYoussefSdiek@users.noreply.github.com>
2023-09-18 01:45:36 +0300

    Fix code in 4-cities_by_state.py file

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,26 +10,21 @@ if __name__ == "__main__":
     database = sys.argv[3]
 
     # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=database)
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
 
-    # Create a cursor object to interact with the database
+    # create cursor to exec queries using SQL; join two tables for all info
     cursor = db.cursor()
+    sql_cmd = """SELECT cities.id, cities.name, states.name
+                 FROM states
+                 INNER JOIN cities ON states.id = cities.state_id
+                 ORDER BY cities.id ASC"""
+    cursor.execute(sql_cmd)
 
-    query = """SELECT cities.id, cities.name, states.name
-               FROM states
-               INNER JOIN cities ON states.id = cities.state_id
-               ORDER BY cities.id ASC"""
-    # Execute the query to retrieve cities with their corresponding state names
-    cursor.execute(query)
-
-    # Fetch all the results
-    results = cursor.fetchall()
-
-    # Display the results
-    for row in results:
+    for row in cursor.fetchall():
         print(row)
-
-    # Close the cursor and database connection
     cursor.close()
     db.close()
