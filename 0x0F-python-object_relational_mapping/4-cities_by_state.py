fbd42c783f641606727cb3380d9cf25360b2c7e0
mahmoudyoussefsdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-09-17 15:54:40 -0700

    Update permission for 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python3
-"""
-return info from both tables (tables 'cities' 'states)
-"""
-
+"""Module to retrieve and display cities from a MySQL database"""
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
+    # Retrieve command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-    # connect to database
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=database)
 
-    # create cursor to exec queries using SQL; join two tables for all info
+    # Create a cursor object to interact with the database
     cursor = db.cursor()
-    sql_cmd = """SELECT cities.id, cities.name, states.name
+
+    query = """SELECT cities.id, cities.name, states.name
                  FROM states
                  INNER JOIN cities ON states.id = cities.state_id
                  ORDER BY cities.id ASC"""
-    cursor.execute(sql_cmd)
+    # Execute the query to retrieve cities with their corresponding state names
+    cursor.execute(query)
 
-    for row in cursor.fetchall():
+    # Fetch all the results
+    results = cursor.fetchall()
+
+    # Display the results
+    for row in results:
         print(row)
+
+    # Close the cursor and database connection
     cursor.close()
     db.close()
