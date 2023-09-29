ce22407beb2fdd63866dea097150e9a194668229
mahmoudyoussefsdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-09-28 18:13:04 -0700

    Create 6-peak.py file

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module contains a function that finds a peak in a list
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    This function finds a peak in a list of unsorted integers.
+
+    Args:
+        list_of_integers (list): List of unsorted integers.
+    """
+    if not list_of_integers:
+        return None
+
+    low = 0
+    high = len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+
+    return list_of_integers[low]
