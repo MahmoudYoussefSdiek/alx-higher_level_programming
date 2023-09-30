edfa2b4e753e0827bdfaa3dbdfa5963a8795afa2
mahmoudyoussefsdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-09-29 17:21:24 -0700

    Create 4-hbtn_status.py file

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This script fetches https://alx-intranet.hbtn.io/status using
+the requests package.
+"""
+
+import requests
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+response = requests.get(url)
+content = response.text
+
+print('Body response:')
+print("\t- type: {}".format(type(content)))
+print("\t- content: {}".format(content))
