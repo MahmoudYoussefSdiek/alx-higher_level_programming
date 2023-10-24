2f6b8133207ce38bb3693f5918d9ac25035445fb
Mahmoud Youssef <111357017+MahmoudYoussefSdiek@users.noreply.github.com>
2023-10-25 00:57:01 +0300

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -9,9 +9,10 @@ const apiUrl = process.argv[2];
 const characterId = 18;
 
 request(apiUrl, (error, response, body) => {
-  if (error) console.error(error);
+  if (!error) {
   const films = JSON.parse(body).results;
   const count = films.filter(film => film.characters.includes(
     `https://swapi-api.alx-tools.com/api/people/${characterId}/`)).length;
   console.log(count);
+  }
 });
