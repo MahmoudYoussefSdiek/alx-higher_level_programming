6503d5c84484cb9b0bbb610b13a85b7784b0aded
mahmoudyoussefsdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-10-24 16:16:34 -0700

    Create 6-completed_tasks.js file

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+/*
+Node.js script that computes the number of tasks completed by user id.
+*/
+
+const request = require('request');
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) console.error(error);
+  const todos = JSON.parse(body);
+  const completed = todos.filter(todo => todo.completed);
+  const completedByUser = completed.reduce((acc, todo) => {
+    if (acc[todo.userId]) {
+      acc[todo.userId]++;
+    } else {
+      acc[todo.userId] = 1;
+    }
+    return acc;
+  }, {});
+  console.log(completedByUser);
+});
