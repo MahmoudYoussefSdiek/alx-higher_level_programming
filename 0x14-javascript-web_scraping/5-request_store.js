e1f3f6335e84c27ea0abc680ad982b5156f1b485
mahmoudyoussefsdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-10-24 15:27:11 -0700

    Create 5-request_store.js file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/*
+Node.js script that gets the contents of a webpage and stores it in a file.
+*/
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) console.error(error);
+  fs.writeFile(filePath, body, { encoding: 'utf-8' }, (error) => {
+    if (error) console.error(error);
+  });
+});
