253d136fc917ef6617a650e1fb1b261c6cfc0c74
mahmoudyoussefsdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-10-24 14:10:17 -0700

    Create 1-writeme.js file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/*
+Node.js script that writes a string to a file.
+*/
+
+const fs = require('fs');
+const filePath = process.argv[2];
+const fileContent = process.argv[3];
+
+fs.writeFile(filePath, fileContent, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(`File ${filePath} written successfully`);
+});
