e9ceda986d0f923361bc9744ce51b5d7c48afd84
Mahmoud Youssef <111357017+MahmoudYoussefSdiek@users.noreply.github.com>
2023-10-25 01:01:31 +0300

    Fix code in 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -9,7 +9,7 @@ const apiUrl = process.argv[2];
 const characterId = 18;
 
 request(apiUrl, (error, response, body) => {
-  if (!error) console.error(error);
+  if (error) console.error(error);
   const films = JSON.parse(body).results;
   const count = films.filter(film => film.characters.includes(
     `https://swapi-api.alx-tools.com/api/people/${characterId}/`)).length;
